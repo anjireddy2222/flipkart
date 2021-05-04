5446c773c93a265dacf79823cfdcec6720ca15d6
Anji reddy <>
2021-05-04 08:22:50 +0530

    nav bar

--- src/components/Nav/TextNav.js
+++ src/components/Nav/TextNav.js
@@ -10,40 +10,55 @@ const TextNav = () => {
     }
   }
 
+  const setUrl = (link) => {
+    if (link.isDropDown == true) {
+      return "#";
+    } else {
+      return link.url;
+    }
+  };
+
+  const setDataToggle = (link) => {
+    return link.isDropDown == true ? "dropdown" : "";
+  };
+  const setHasPopup = (link) => {
+    return link.isDropDown == true ? "true" : "";
+  };
+  const setExpanded = (link) => {
+    return link.isDropDown == true ? "false" : "";
+  };
+
+
   return (
     <div className="container-fluid kart-shadow navbar navbar-expand-lg ">
       <div className="container">
         <div className="navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {
-              menuTextLinks.map( (link, i) => 
-                <li className="nav-item" key={i}>
-                  <a
-                    className={setClassNames(link.isDropDown)}
-                    href="#"
-                    tabIndex="-1"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                    id={ "dropDownId" + i }
-                    >
-                    { link.text }
-                  </a>
-                  { 
-                    link.isDropDown && 
-                    <div className="dropdown-menu"  aria-labelledby={ "dropDownId" + i } >
-                      {
-                        link.dropDownValues.map( (subLink, j) =>
-                        <a href="#" className="dropdown-item" key={j}>
-                          { subLink.text }
-                        </a>  
-                        )
-                      }
-                    </div>
-                  }
-                </li>
-              )
-            }
+            {menuTextLinks.map((link, i) => (
+              <li className="nav-item" key={i}>
+                <a
+                  className={setClassNames(link.isDropDown)}
+                  href={setUrl(link)}
+                  tabIndex="-1"
+                  data-toggle={setDataToggle(link)}
+                  aria-haspopup={setHasPopup(link)}
+                  aria-expanded={setExpanded(link)}
+                  id={"dropDownId" + i}>
+                  {link.text}
+                </a>
+                {link.isDropDown && (
+                  <div
+                    className="dropdown-menu shadow border-0"
+                    aria-labelledby={"dropDownId" + i}>
+                    {link.dropDownValues.map((subLink, j) => (
+                      <a href={subLink.url} className="dropdown-item" key={j}>
+                        {subLink.text}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/components/Nav/Utils.js
+++ src/components/Nav/Utils.js
@@ -2,40 +2,41 @@ export const menuTextLinks = [
   {
     text: "Electronics",
     isDropDown: true,
-    dropDownValues: [{ text: "link 1" }, { text: "link 2" }],
+    dropDownValues: [{ text: "link 1", url: "products?category=link1" }, { text: "link 2", url: "products?category=link2"  }],
   },
 
   {
     text: "TVs & Appliances",
     isDropDown: true,
-    dropDownValues: [{ text: "link 3" }, { text: "link 4" }],
+    dropDownValues: [{ text: "link 3", url: "products?category=link4"  }, { text: "link 4", url: "products?category=link5"  },
+    { text: "link 31", url: "products?category=link41"  }, { text: "link 41", url: "products?category=link51"  }],
   },
   {
     text: "Men",
     isDropDown: true,
-    dropDownValues: [{ text: "link 5" }, { text: "link 6" }],
+    dropDownValues: [{ text: "link 5", url: "products?category=link6"  }, { text: "link 6", url: "products?category=link7"  }],
   },
   {
     text: "Women",
     isDropDown: true,
-    dropDownValues: [{ text: "link 7" }, { text: "link 8" }],
+    dropDownValues: [{ text: "link 7", url: "products?category=link8"  }, { text: "link 8", url: "products?category=link9"  }],
   },
   {
     text: "Baby & Kids",
     isDropDown: true,
-    dropDownValues: [{ text: "link 9" }, { text: "link 10" }],
+    dropDownValues: [{ text: "link 9", url: "products?category=link10"  }, { text: "link 10", url: "products?category=link10"  }],
   },
   {
     text: "Home & Furniture",
     isDropDown: true,
-    dropDownValues: [{ text: "link 11" }, { text: "link 12" }],
+    dropDownValues: [{ text: "link 11", url: "products?category=link11"  }, { text: "link 12", url: "products?category=link12"  }],
   },
   {
     text: "Sports, Books & More",
     isDropDown: true,
-    dropDownValues: [{ text: "link 13" }, { text: "link 14" }],
+    dropDownValues: [{ text: "link 13", url: "products?category=link13"  }, { text: "link 14", url: "products?category=link14"  }],
   },
-  { text: "Flights", isDropDown: false },
-  { text: "Offer Zone", isDropDown: false },
-  { text: "Grocery", isDropDown: false },
+  { text: "Flights", isDropDown: false, url: "products?category=flights"  },
+  { text: "Offer Zone", isDropDown: false, url: "products?category=offer-zone"  },
+  { text: "Grocery", isDropDown: false, url: "products?category=grocery"  },
 ];
